58fd8410b8b24d92030de7010fcb5bd9809b4393
jbaxax <walterjave92@gmail.com>
2025-02-15 12:33:23 -0500

    ➕ add component header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Header from "@/components/header";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -19,8 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${urbanist.className}`}>
+      <body className={`${urbanist.className} `}>
         <Navbar />
+        <Header/>
         {children}
       </body>
     </html>
---
+++ components/header/index.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import MotionTransition from "../transition-component";
+import { socialNetworks } from "@/data";
+
+export default function Header() {
+  return (
+    <MotionTransition
+      position="bottom"
+      className="absolute z-40 inline-block w-full top-5 md:top-10"
+    >
+      <header>
+        <div className="container justify-between max-w-6xl mx-auto md:flex">
+          <Link href="/">
+            <h1 className="my-4 text-4xl font-bold text-center md:text-left">Walter<span className="text-secondary">Jave</span></h1>
+           
+            
+          </Link>
+          <div className="flex items-center justify-center gap-7">
+            {socialNetworks.map(({ logo, src, id }) => (
+              <Link href={src} key={id} target="_blank" className="transition-all duration-300 hover:text-secondary">
+                {logo}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </header>
+    </MotionTransition>
+  );
+}
